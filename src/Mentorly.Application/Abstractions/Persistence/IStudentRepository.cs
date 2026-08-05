38be0f0a2e78f6951ac1ff9e0773debbbd3f5c7f
2005lohammy <2005lohammy@gmail,com>
2026-08-05 19:34:34 -0400

    feat: Implementa operaciones CRUD en StudentRepository e IStudentRepository

--- src/Mentorly.Application/Abstractions/Persistence/IStudentRepository.cs
+++ src/Mentorly.Application/Abstractions/Persistence/IStudentRepository.cs
@@ -1,6 +1,18 @@
+using Mentorly.Domain.Entities;
+
 namespace Mentorly.Application.Abstractions.Persistence;
 
 public interface IStudentRepository
 {
     Task<bool> ExistsAsync(Guid studentId, CancellationToken cancellationToken = default);
+
+    Task<Student?> GetByIdAsync(Guid studentId, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    void Add(Student student);
+
+    void Update(Student student);
+
+    void Delete(Student student);
 }
