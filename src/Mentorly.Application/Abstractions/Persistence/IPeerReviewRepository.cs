eb54456c352a7c28ee9e5032d7753d3e4ec7ae4a
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-05 19:10:42 -0400

    Feat: Agrega los metodos Get, GetById, Update y Delete  en el IPeerReviewRepository

--- src/Mentorly.Application/Abstractions/Persistence/IPeerReviewRepository.cs
+++ src/Mentorly.Application/Abstractions/Persistence/IPeerReviewRepository.cs
@@ -4,9 +4,11 @@ namespace Mentorly.Application.Abstractions.Persistence;
 
 public interface IPeerReviewRepository
 {
+    Task<IReadOnlyList<PeerReview>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<PeerReview?> GetByIdAsync(Guid peerReviewId, CancellationToken cancellationToken = default);
     Task<bool> HasReviewerAlreadyReviewedAsync(Guid submissionId, Guid reviewerStudentId, CancellationToken cancellationToken = default);
-
     Task<int> CountApprovalsForSubmissionAsync(Guid submissionId, CancellationToken cancellationToken = default);
-
     Task AddAsync(PeerReview review, CancellationToken cancellationToken = default);
+    void Update(PeerReview review);
+    void Delete(PeerReview review);
 }
