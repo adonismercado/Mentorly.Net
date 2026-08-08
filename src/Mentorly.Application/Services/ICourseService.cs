d1a4c2ad2fcca0c419b7a65ac2489d92d336c7ca
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-07 23:34:21 -0400

    actualiza interfazo de CourseServices

--- src/Mentorly.Application/Services/ICourseService.cs
+++ src/Mentorly.Application/Services/ICourseService.cs
@@ -5,8 +5,10 @@ namespace Mentorly.Application.Services;
 public interface ICourseService
 {
     Task<CourseDto[]> GetAllCoursesAsync(CancellationToken cancellationToken = default);
-    Task<CourseDto?> GetCourseByIdAsync(Guid courseId, CancellationToken cancellationToken = default);
-    Task<CourseDto> CreateCourseAsync(CreateCourseDto dto, CancellationToken cancellationToken = default);
-    Task<bool> UpdateCourseAsync(Guid courseId, UpdateCourseDto dto, CancellationToken cancellationToken = default);
-    Task<bool> DeleteCourseAsync(Guid courseId, CancellationToken cancellationToken = default);
+    Task<CourseDetailDto?> GetCourseByIdAsync(Guid courseId, CancellationToken cancellationToken = default);
+    Task<CourseContentDto?> GetCourseContentAsync(Guid courseId, CancellationToken cancellationToken = default);
+    Task<CourseDto?> CreateCourseAsync(Guid adminId, CreateCourseDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateCourseAsync(Guid adminId, Guid courseId, UpdateCourseDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdatePublicationAsync(Guid adminId, Guid courseId, bool isPublished, CancellationToken cancellationToken = default);
+    Task<bool> DeleteCourseAsync(Guid adminId, Guid courseId, CancellationToken cancellationToken = default);
 }
