3d8fa8e3e2c7fec748ba7b390cc4db119c119da0
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-11 22:55:48 -0400

    agrega consulta administrativa de entregas escaladas en ISubmissionService

--- src/Mentorly.Application/Services/ISubmissionService.cs
+++ src/Mentorly.Application/Services/ISubmissionService.cs
@@ -5,6 +5,7 @@ namespace Mentorly.Application.Services;
 public interface ISubmissionService
 {
     Task<SubmissionDto[]> GetAllSubmissionsAsync(CancellationToken cancellationToken = default);
+    Task<SubmissionDto[]> GetEscalatedSubmissionsAsync(Guid adminId, CancellationToken cancellationToken = default);
     Task<SubmissionDto?> GetSubmissionByIdAsync(Guid submissionId, CancellationToken cancellationToken = default);
     Task<SubmissionDto> CreateSubmissionAsync(Guid enrollmentId, Guid activityId, CreateSubmissionDto dto, CancellationToken cancellationToken = default);
     Task<bool> UpdateSubmissionAsync(Guid submissionId, UpdateSubmissionDto dto, CancellationToken cancellationToken = default);
